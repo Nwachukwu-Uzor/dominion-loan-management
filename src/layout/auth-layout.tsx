e4f13417bacaa1d4c4cbd43f4415f126dac67622
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-06-20 21:38:36 +0100

    update auth layout

--- src/layout/auth-layout.tsx
+++ src/layout/auth-layout.tsx
@@ -5,18 +5,18 @@ import authIllustration from "@/assets/images/auth-bg.jpg";
 export const AuthLayout = () => {
   return (
     <main className="grid grid-cols-1 lg:grid-cols-2">
-      <section className="hidden lg:flex justify-center items-center min-h-screen bg-gray-200">
+      <section className="hidden lg:flex justify-center items-center min-h-screen">
         <img
           src={authIllustration}
           alt="Auth"
-          className="w-3/5 rounded-sm inline-block"
+          className="w-full h-screen object-center inline-block"
         />
       </section>
       <section className="min-h-screen flex items-center justify-center w-[95%] mx-auto max-w-[400px]">
         <img
           src={logo}
           alt="Auth"
-          className="absolute top-4 right-4 h-6 lg:h-8"
+          className="absolute top-4 right-4 h-6 lg:h-12"
         />
         <article className="w-full">
           <h2 className="scroll-m-20 text-3xl font-bold tracking-tight mb-12">
